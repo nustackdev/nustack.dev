24b4b118ce1cc72f239005b998d7c062df3c2ccb
Gor <arakelyangor10@gmail.com>
2026-08-04 16:56:03 +0400

    Add landing page: hero + sections scaffold with logo layer

---
+++ app/kickasslanding/layout.tsx
@@ -0,0 +1,11 @@
+import { HomeLayout } from 'fumadocs-ui/layouts/home';
+import { baseOptions } from '@/lib/layout.shared';
+import { FloatingNav } from '@/components/FloatingNav';
+
+export default function Layout({ children }: LayoutProps<'/kickasslanding'>) {
+  return (
+    <HomeLayout {...baseOptions()} slots={{ header: FloatingNav }}>
+      {children}
+    </HomeLayout>
+  );
+}
